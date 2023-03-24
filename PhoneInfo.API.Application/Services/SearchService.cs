cb70eff524443df9e50fcde339ffc315467f5773
Quoc Thao <thaodlq2@fpt.com.vn>
2023-03-24 10:55:51 +0700

    Add Search api

---
+++ PhoneInfo.API.Application/Services/SearchService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using PhoneInfo.API.Domain.HttpRequest;
+using PhoneInfo.API.Domain.Interfaces;
+using System.Net;
+
+namespace PhoneInfo.API.Application.Services
+{
+	public class SearchService : ServiceBase, ISearchService
+	{
+		public SearchService(IHttpService httpService, IConfiguration configuration) : base(httpService, configuration)
+		{
+		}
+
+		public async Task<(string, HttpStatusCode)> SearchAll(string keyword)
+		{
+			return await GetResultAsync($"results.php3?sQuickSearch=yes&sName={keyword}", HttpMethod.Get);
+		}
+	}
+}
---
+++ PhoneInfo.API.Core/Interfaces/ISearchService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace PhoneInfo.API.Domain.Interfaces
+{
+	public interface ISearchService
+	{
+		Task<(string, HttpStatusCode)> SearchAll(string keyword);
+	}
+}
